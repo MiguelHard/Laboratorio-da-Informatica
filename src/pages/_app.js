65bd56c2f138b062f459bec3f52699788b054a7d
MiguelHard <migueldeveloperhard@gmail.com>
2025-02-21 10:12:58 -0400

    adicionando analises da vercel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
+ 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
